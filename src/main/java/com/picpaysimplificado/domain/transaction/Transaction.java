3fe5087d47e4fb5b8ad5610340eb749491e7b393
pedrothomaz75 <pedroimachida@gmail.com>
2024-03-05 15:40:50 -0300

    added JoinColumn

--- src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -1,6 +1,7 @@
 package com.picpaysimplificado.domain.transaction;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import com.picpaysimplificado.domain.user.User;
 
@@ -8,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -27,9 +29,12 @@ public class Transaction {
     private Long id;
     private BigDecimal amount;
     @ManyToOne // Um usuário pode fazer várias transações, mas um usuário pode enviar dinheiro para uma única pessoa na mesma operação
+    @JoinColumn(name="sender_id")
     private User sender; // A pessoa que envia
     @ManyToOne
+    @JoinColumn(name="reciever_id")
     private User receiver;
+    private LocalDateTime timestamp;
 
     
 }
